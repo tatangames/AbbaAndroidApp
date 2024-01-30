b7257e10f2add36763931738cde3135b8a711e78
tatangames <54828743+tatangames@users.noreply.github.com>
2024-01-30 14:54:57 -0600

    soporte notificaciones

--- app/src/main/java/com/tatanstudios/abbaappandroid/MainActivity.java
+++ app/src/main/java/com/tatanstudios/abbaappandroid/MainActivity.java
@@ -10,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        *
+        * boolean hasNotificationsEnabled = OneSignal.getPermissionSubscriptionState().getPermissionStatus().getEnabled();
+
+* String userId = OneSignal.getPermissionSubscriptionState().getSubscriptionStatus().getUserId();
+Log.d("OneSignal", "User ID: " + userId);
+*
+if (hasNotificationsEnabled) {
+    // El usuario tiene permitido notificaciones
+} else {
+    // El usuario no tiene permitido notificaciones
+}
+        *
+        * */
     }
 }
---
+++ app/src/main/java/com/tatanstudios/abbaappandroid/extras/SistemaController.java
@@ -0,0 +1,42 @@
+package com.tatanstudios.abbaappandroid.extras;
+
+import android.app.Application;
+
+import com.onesignal.Continue;
+import com.onesignal.OneSignal;
+import com.onesignal.debug.LogLevel;
+
+public class SistemaController extends Application {
+
+    // ID DE API ABBA APP
+    private static final String ONESIGNAL_APP_ID = "cd253220-f1b1-4b46-9307-37cf176768a9";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Verbose Logging set to help debug issues, remove before releasing your app.
+        OneSignal.getDebug().setLogLevel(LogLevel.VERBOSE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this, ONESIGNAL_APP_ID);
+
+        // requestPermission will show the native Android notification permission prompt.
+        // NOTE: It's recommended to use a OneSignal In-App Message to prompt instead.
+        OneSignal.getNotifications().requestPermission(true, Continue.with(r -> {
+            if (r.isSuccess()) {
+                if (r.getData()) {
+                    // `requestPermission` completed successfully and the user has accepted permission
+                }
+                else {
+                    // `requestPermission` completed successfully but the user has rejected permission
+                }
+            }
+            else {
+                // `requestPermission` completed unsuccessfully, check `r.getThrowable()` for more info on the failure reason
+            }
+        }));
+    }
+
+
+}
