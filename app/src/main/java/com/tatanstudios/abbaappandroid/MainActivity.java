916751c84ec23811e6fda8eb28411587ab58ece6
tatangames <54828743+tatangames@users.noreply.github.com>
2024-01-29 20:27:52 -0600

    inicio app limpia

---
+++ app/src/main/java/com/tatanstudios/abbaappandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tatanstudios.abbaappandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
