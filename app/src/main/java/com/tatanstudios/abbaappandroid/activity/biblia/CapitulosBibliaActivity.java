b54290c2bda3c91b957ddf62c81acf6702059ca7
tatangames <54828743+tatangames@users.noreply.github.com>
2024-02-21 15:49:43 -0600

    mejor usar recyclerview

--- app/src/main/java/com/tatanstudios/abbaappandroid/activity/biblia/CapitulosBibliaActivity.java
+++ app/src/main/java/com/tatanstudios/abbaappandroid/activity/biblia/CapitulosBibliaActivity.java
@@ -4,22 +4,26 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.tatanstudios.abbaappandroid.R;
-import com.tatanstudios.abbaappandroid.adaptadores.comunidad.AdaptadorSolicitudPendientesEnviadas;
+import com.tatanstudios.abbaappandroid.modelos.biblia.grupos.ModeloGrupo;
+import com.tatanstudios.abbaappandroid.modelos.biblia.grupos.ModeloSubGrupo;
 import com.tatanstudios.abbaappandroid.network.ApiService;
 import com.tatanstudios.abbaappandroid.network.RetrofitBuilder;
 import com.tatanstudios.abbaappandroid.network.TokenManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import es.dmoral.toasty.Toasty;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -28,8 +32,6 @@ import io.reactivex.schedulers.Schedulers;
 
 public class CapitulosBibliaActivity extends AppCompatActivity {
 
-
-    private RecyclerView recyclerView;
 
     private RelativeLayout rootRelative;
 
@@ -47,17 +49,24 @@ public class CapitulosBibliaActivity extends AppCompatActivity {
 
     private int idbiblia = 0;
 
+
+
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capitulos_biblia);
-        recyclerView = findViewById(R.id.recyclerView);
         imgFlechaAtras = findViewById(R.id.imgFlechaAtras);
         txtToolbar = findViewById(R.id.txtToolbar);
         rootRelative = findViewById(R.id.rootRelative);
 
         txtToolbar.setText(getString(R.string.referencias));
-
 
         if (getIntent().getExtras() != null) {
             Bundle bundle = getIntent().getExtras();
@@ -79,6 +88,17 @@ public class CapitulosBibliaActivity extends AppCompatActivity {
         onBackPressedDispatcher = getOnBackPressedDispatcher();
 
 
+
+
+
+        // Llenar el modelo de datos con marcas y modelos
+        List<ModeloGrupo> listaDeGrupos = llenarDatos();
+
+
+
+
+
+
         imgFlechaAtras.setOnClickListener(v -> {
             volverAtras();
         });
@@ -92,8 +112,16 @@ public class CapitulosBibliaActivity extends AppCompatActivity {
 
         onBackPressedDispatcher.addCallback(onBackPressedCallback);
 
-        apiBuscarCapitulos();
+       // apiBuscarCapitulos();
+
+
+
+
     }
+
+
+
+
 
 
     private void apiBuscarCapitulos(){
@@ -129,6 +157,49 @@ public class CapitulosBibliaActivity extends AppCompatActivity {
                                 })
         );
     }
+
+
+    private List<ModeloGrupo> llenarDatos(){
+
+        List<ModeloGrupo> listaDeGrupos = new ArrayList<>();
+
+        List<ModeloSubGrupo> modeloSamsung = new ArrayList<>();
+        modeloSamsung.add(new ModeloSubGrupo(1, "A1"));
+        modeloSamsung.add(new ModeloSubGrupo(2, "A2"));
+        modeloSamsung.add(new ModeloSubGrupo(3, "A3"));
+        modeloSamsung.add(new ModeloSubGrupo(4, "A4"));
+        modeloSamsung.add(new ModeloSubGrupo(5, "A5"));
+
+        ModeloGrupo samsung = new ModeloGrupo("Samsung", modeloSamsung);
+        listaDeGrupos.add(samsung);
+
+        List<ModeloSubGrupo> modeloApple = new ArrayList<>();
+        modeloApple.add(new ModeloSubGrupo(6, "P1"));
+        modeloApple.add(new ModeloSubGrupo(7, "P2"));
+        modeloApple.add(new ModeloSubGrupo(8, "P3"));
+        modeloApple.add(new ModeloSubGrupo(9, "P4"));
+        modeloApple.add(new ModeloSubGrupo(10, "P5"));
+
+        ModeloGrupo apple = new ModeloGrupo("Apple", modeloApple);
+        listaDeGrupos.add(apple);
+
+        List<ModeloSubGrupo> modeloMotorola = new ArrayList<>();
+        modeloMotorola.add(new ModeloSubGrupo(11, "M1"));
+        modeloMotorola.add(new ModeloSubGrupo(12, "M2"));
+        modeloMotorola.add(new ModeloSubGrupo(13, "M3"));
+        modeloMotorola.add(new ModeloSubGrupo(14, "M4"));
+        modeloMotorola.add(new ModeloSubGrupo(15, "M5"));
+
+        ModeloGrupo motorola = new ModeloGrupo("Motorola", modeloMotorola);
+        listaDeGrupos.add(motorola);
+
+        return listaDeGrupos;
+    }
+
+
+
+
+
 
     private void volverAtras(){
         finish();
--- app/src/main/java/com/tatanstudios/abbaappandroid/modelos/biblia/grupos/ModeloGrupo.java
+++ app/src/main/java/com/tatanstudios/abbaappandroid/modelos/biblia/grupos/ModeloGrupo.java
@@ -1,11 +1,19 @@
 package com.tatanstudios.abbaappandroid.modelos.biblia.grupos;
 
+import java.util.List;
+
 public class ModeloGrupo {
 
     private String titulo;
+    private List<ModeloSubGrupo> modeloSubGrupos;
 
-    public ModeloGrupo(String titulo) {
+    public ModeloGrupo(String titulo, List<ModeloSubGrupo> modeloSubGrupos) {
         this.titulo = titulo;
+        this.modeloSubGrupos = modeloSubGrupos;
+    }
+
+    public List<ModeloSubGrupo> getModeloSubGrupos() {
+        return modeloSubGrupos;
     }
 
     public String getTitulo() {
